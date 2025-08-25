ca4f88485995a539f94f570f420028ee196ad8f1
ibrahim shehta <iabdelaziz@elm.sa>
2025-08-25 22:01:51 +0300

    first commit

---
+++ src/main/java/com/pipeline/example/pipeline_example/HelloController.java
@@ -0,0 +1,20 @@
+package com.pipeline.example.pipeline_example;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/hello")
+public class HelloController {
+
+
+    @Value("${config.message}")
+    private String msg;
+
+    @GetMapping("/msg")
+    public String hello() {
+        return msg;
+    }
+}
---
+++ src/main/java/com/pipeline/example/pipeline_example/PipelineExampleApplication.java
@@ -0,0 +1,13 @@
+package com.pipeline.example.pipeline_example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PipelineExampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PipelineExampleApplication.class, args);
+	}
+
+}
