25fa5fa4874557d718b6c1e774d43c5cd0a3bb87
ibrahim shehta <iabdelaziz@elm.sa>
2025-08-26 22:09:16 +0300

    add health check api

---
+++ src/main/java/com/pipeline/example/pipeline_example/HealthCheckController.java
@@ -0,0 +1,13 @@
+package com.pipeline.example.pipeline_example;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HealthCheckController {
+
+    @GetMapping
+    public String health() {
+        return "UP";
+    }
+}
