a291454827550a4346f634637534f988ba2abec6
ibrahim shehta <iabdelaziz@elm.sa>
2025-08-26 22:39:35 +0300

    trigger the pipeline

--- src/main/java/com/pipeline/example/pipeline_example/HealthCheckController.java
+++ src/main/java/com/pipeline/example/pipeline_example/HealthCheckController.java
@@ -6,6 +6,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class HealthCheckController {
 
+    //
     @GetMapping
     public String health() {
         return "UP";
